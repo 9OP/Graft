46cbdc345a29d958f02caf3b0a61335a757e14be
martin <martin-guyard@live.fr>
2022-10-26 13:25:41 +0200

    Updated rpcClient port

--- app/port/rpcClient.go
+++ app/port/rpcClient.go
@@ -2,25 +2,33 @@ package port
 
 import (
 	"fmt"
-	"graft/app/adapter"
 	"graft/app/adapter/rpc"
 	"graft/app/domain/entity"
 )
 
-// Repository in use case
+// Repository in use case runner
 //
-// type RpcClientPort interface {
+// type Repo interface {
 // 	AppendEntries(peer entity.Peer, input entity.AppendEntriesInput) (entity.AppendEntriesOutput, error)
 // 	RequestVote(peer entity.Peer, input entity.RequestVoteInput) (entity.RequestVoteOutput, error)
 // }
 
-type RpcClientPort struct{}
+type RpcClientAdapter interface {
+	AppendEntries(target string, input *rpc.AppendEntriesInput) (*rpc.AppendEntriesOutput, error)
+	RequestVote(target string, input *rpc.RequestVoteInput) (*rpc.RequestVoteOutput, error)
+}
 
-func (p *RpcClientPort) AppendEntries(peer entity.Peer, input *entity.AppendEntriesInput) (*entity.AppendEntriesOutput, error) {
-	rpcClient := adapter.RpcClient{}
+type rpcClientPort struct {
+	adapter RpcClientAdapter
+}
 
+func NewRpcClientPort(adapter RpcClientAdapter) *rpcClientPort {
+	return &rpcClientPort{adapter}
+}
+
+func (p *rpcClientPort) AppendEntries(peer entity.Peer, input *entity.AppendEntriesInput) (*entity.AppendEntriesOutput, error) {
 	target := fmt.Sprintf("%s:%s", peer.Host, peer.Port)
-	output, err := rpcClient.AppendEntries(target, &rpc.AppendEntriesInput{
+	output, err := p.adapter.AppendEntries(target, &rpc.AppendEntriesInput{
 		Term:         input.Term,
 		LeaderId:     input.LeaderId,
 		PrevLogIndex: input.PrevLogIndex,
@@ -39,12 +47,21 @@ func (p *RpcClientPort) AppendEntries(peer entity.Peer, input *entity.AppendEntr
 	}, nil
 }
 
-/*
-Port implementation needs to:
-- inject use case
-- inject adapter
-- transform adpter input to entity
-- runs entity trhough use case
-- transform use case respose to adapter output
-- returns
-*/
+func (p *rpcClientPort) RequestVote(peer entity.Peer, input *entity.RequestVoteInput) (*entity.RequestVoteOutput, error) {
+	target := fmt.Sprintf("%s:%s", peer.Host, peer.Port)
+	output, err := p.adapter.RequestVote(target, &rpc.RequestVoteInput{
+		Term:         input.Term,
+		CandidateId:  input.CandidateId,
+		LastLogIndex: input.LastLogIndex,
+		LastLogTerm:  input.LastLogTerm,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &entity.RequestVoteOutput{
+		Term:        output.Term,
+		VoteGranted: output.VoteGranted,
+	}, nil
+}
